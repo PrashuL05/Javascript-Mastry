10e99d3af41361cd6ba38bab6197073f33a68ab1
PrashuL05 <133563261+PrashuL05@users.noreply.github.com>
2024-10-19 05:21:49 +0000

    Data types and Comparisions

---
+++ 01_basics/03_dataTypeConvert.js
@@ -0,0 +1,64 @@
+let score = undefined;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+/*
+"33" => 33
+"33abc" => NaN
+true = > 1 , false =>0,
+"" => false , "prashanth" => true
+null => object after convert to number becomes 0.
+undefined => object after convert to number becomes NaN.
+
+*/
+
+let isLoggedIn = "";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn)
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+/*****************************Operations*******************************************/
+let value  = 3
+let negValue = -value
+// console.log(negValue)
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1 = "Hello";
+let str2 = " Prashanth";
+let str3 = str1+str2;
+
+// console.log(str3); // Hello Prashanth
+
+// console.log("1"+2); //12
+// console.log(1+"2") //12
+// console.log(1+"2"+2) //122
+// console.log(1+2+"3") //33
+// console.log(2+3*5%3) // don't use like this use parenthises //res= 2 because 3*5= 15+2 = 17%3 = 2
+// console.log((2+3)*(5%3)) // 5*2 = 10
+
+// console.log(+true) // returns 1
+// console.log(+"") // 0
+
+let counter = 0;
+// console.log(counter);
+// console.log(++counter);
+// console.log(counter++)
+// console.log(counter)
+
+//ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,24 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2<=1)
+// console.log(2==1)
+// console.log(2!=1)
+
+//problem occurs when we compare different type of data typed values
+
+// console.log("2" > 1); //true
+// console.log("02">1) //true
+
+// console.log(null>0); //false
+// console.log(null == 0); //false
+// console.log(null >= 0); //true
+// console.log(null>0) //false
+
+// console.log(undefined>0); //false
+// console.log(undefined == 0); //false
+// console.log(undefined >= 0); //true
+// console.log(undefined >0) //false
+
+// === (strict check ): it checks along with the type
+
