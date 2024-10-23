05caacc9c0c7675a6bc09994f1565a476602cdf7
PrashuL05 <133563261+PrashuL05@users.noreply.github.com>
2024-10-23 17:28:34 +0000

    maths methods

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -28,6 +28,19 @@ const hundreds = 100000000;
 
 //+++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++
 
-console.log(Math);
-console.log(Math.abs(-4));
-console.log(Math.round(4.6));
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.6));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(6,3,12,67,1,23));
+// console.log(Math.max(6,3,12,67,1,23));
+
+console.log(Math.random());
+console.log(Math.random()*10);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max-min+1))+min);
