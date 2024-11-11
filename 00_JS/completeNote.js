dcd26e978de3f453428bab0ffd06cd315fe70cb7
PrashuL05 <133563261+PrashuL05@users.noreply.github.com>
2024-11-11 17:06:12 +0000

    switch case

--- 00_JS/completeNote.js
+++ 00_JS/completeNote.js
@@ -78,4 +78,30 @@ var authenticated = true;
 
 authenticated? console.log("Signout") : console.log("Login")
 
-//L12
+//L12 - switch for role based access
+
+// create an application with following roles:
+// admin - get full access 
+// subadmin - gets access to create/delete courses
+// testprep - gets access to create/delete tests
+// user - gets access to consume content 
+
+var user ="testprep";
+switch(user){
+    case "admin":
+        console.log("get full access");
+        break;
+    case "subadmin":
+        console.log("gets access to create/delete courses");
+        break;
+    case "testprep":
+        console.log("gets access to create/delete tests");
+        break;
+    case "user":
+        console.log("gets access to consume content");
+        break;
+    default:
+        console.log("Please choose the appropriate role");
+        break;
+
+}
